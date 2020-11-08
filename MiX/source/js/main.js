f911cf72927efc77b7bc0488bd054b4697613b77
Alexey Baranov <57197394+phesende@users.noreply.github.com>
2020-11-08 10:04:25 +0300

    Add files via upload

--- MiX/source/js/main.js
+++ MiX/source/js/main.js
@@ -1,3 +1,4 @@
+init();
 $(document).ready(function(){
 
     $('.header__burger').click(function(){
@@ -5,3 +6,18 @@ $(document).ready(function(){
         $('body').toggleClass('lock');
     });
 });
+    function init() {
+
+        if(!isMobile()) {
+            document.write('<script  src="source/js/flip.js"></script>');
+            
+        }
+    }
+    
+    
+    function isMobile() {
+    
+        if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/IEMobile/i))
+                return true;
+            return false;
+    }
